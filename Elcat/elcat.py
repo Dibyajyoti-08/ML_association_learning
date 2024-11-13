17a2ea1c5f57e48d7438232f5a74dfb2a4800684
djena <eng.djena075@gmail.com>
2024-11-13 21:01:56 +0530

    Add new ML concept which is base on association learning

---
+++ Elcat/elcat.py
@@ -0,0 +1,36 @@
+# Installing Package
+import subprocess
+import sys
+
+subprocess.call([sys.executable, '-m', 'pip', 'install', 'apyori'])
+
+# Importing Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from apyori import apriori
+
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+
+
+# Train the Apriori model on the dataset
+rules = apriori(transactions = transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2, max_length = 2)
+
+# Visualization of result
+results = list(rules)
+print(results)
+
+# Put the result well organised into a Pandas DataFrame
+def inspect(results):
+    lhs = [tuple(result[2][0][0])[0] for result in results]
+    rhs = [tuple(result[2][0][1])[0] for result in results]
+    supports = [result[1] for result in results]
+    return list(zip(lhs, rhs, supports))
+resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left Hand Side', 'Right Hand Side', 'Support'])
+
+print("\nSorted Result:")
+print(resultsinDataFrame.nlargest(n = 10, columns = 'Support'))
